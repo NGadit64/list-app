fb0ea3ac1b19b678679008332ae531171b1a8b20
faruq207 <dafig57@gmail.com>
2025-10-14 17:33:27 +0700

    login

--- lib/bindings/history_binding.dart
+++ lib/bindings/history_binding.dart
@@ -4,6 +4,7 @@ import 'package:list_app/controllers/history_controller.dart';
 class HistoryBinding extends Bindings {
   @override
   void dependencies() {
+    //bidning
     Get.lazyPut<HistoryController>(() => HistoryController());
   }
 }
